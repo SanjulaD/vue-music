6e2efe6f3155d1ef589cfe941219c908c4ee680a
SanjulaD <desanjula99@gmail.com>
2022-07-12 11:58:58 +0530

    header and auth component

--- src/main.js
+++ src/main.js
@@ -5,4 +5,9 @@ import store from './store';
 import './index.css';
 import './main.css';
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+
+app.mount('#app');
